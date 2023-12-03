3516533437f4963e39d34d9070d5eeee2a5e34f2
fuga-98 <iwasataisei@gmail.com>
2023-12-03 20:31:07 +0900

    AIアプリ開発入門05章

--- chapter03.py
+++ chapter03.py
@@ -6,14 +6,24 @@ from langchain.schema import (
 )
 
 llm = ChatOpenAI()  # ChatGPT APIを呼んでくれる機能
-message = "Hi, ChatGPT! あなたはコロ助ですか？"  # あなたの質問をここに書く
+# message = "Hi, ChatGPT! あなたはコロ助ですか？"  # あなたの質問をここに書く
 
+# messages = [
+#     # 設定を決める指示
+#     SystemMessage(content="語尾に「ナリ」をつけてください"),
+#     HumanMessage(content=message)
+# ]
+# response = llm(messages)
+# print(response)
+
+message = "ChatGPTとStreamlitでAIアプリを作る本を書く。タイトルを1個考えて。"
 messages = [
-    # 設定を決める指示
-    SystemMessage(content="語尾に「ナリ」をつけてください"),
+    SystemMessage(content="You are a helpful assistant."),
     HumanMessage(content=message)
 ]
-response = llm(messages)
-print(response)
-
+for temperature in [0, 1, 2]:
+    print(f'==== temp: {temperature}')
+    llm = ChatOpenAI(temperature=temperature)
+    for i in range(3):
+        print(llm(messages).content)
 # content='Hello! How can I assist you today?' additional_kwargs={} example=False
